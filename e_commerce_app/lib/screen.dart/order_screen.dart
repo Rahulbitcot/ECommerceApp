e586de9f5acd6785434ae22c7dd1bfde0753f667
Rahulbitcot <rahulmukati@bitcot.com>
2025-02-10 16:44:22 +0530

    Fixed Order Screen UI

--- e_commerce_app/lib/screen.dart/order_screen.dart
+++ e_commerce_app/lib/screen.dart/order_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+
 import 'package:e_commerce_app/models/cart.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -63,65 +64,66 @@ Widget cardListView(
     int index, String title, String imgUrl, String price, Color bgColor) {
   return Card(
     margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
-    child: Row(
-      children: [
-        Container(
-          decoration: const BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(20)),
+    child: Padding(
+      padding: const EdgeInsets.only(bottom: 5 , right: 5),
+      child: Row(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+            margin: const EdgeInsets.all(10),
+            child: SizedBox(
+              width: 50,
+              height: 50,
+              child: Image.network(imgUrl),
+            ),
           ),
-          margin: const EdgeInsets.all(10),
-          padding: const EdgeInsets.all(10),
-          child: SizedBox(
-            width: 80,
-            height: 80,
-            child: Image.network(imgUrl),
-          ),
-        ),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                title,
-                style: const TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
                 ),
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-              ),
-              const SizedBox(height: 10),
-              const Text(
-                "Size: M",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18,
+                const SizedBox(height: 3),
+                const Text(
+                  "Size: M",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 5),
-              Row(
-                children: [
-                  Text(
-                    "INR: $price",
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 15,
+                const SizedBox(height: 3),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      "INR: $price",
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
                     ),
-                  ),
-                  const Spacer(),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text("Track Order"),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 5),
-            ],
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: const Text("Track Order"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     ),
   );
 }
